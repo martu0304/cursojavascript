c87eb5b36f5e5ef97d58b89582234b9ae54c9a59
Martina Harbi <martinaharbi@gmail.com>
2024-12-12 18:05:56 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// let alumnoUno = Number(prompt("Ingrese la nota del alumno 1"));
+// let alumnoDos = Number(prompt("Ingrese la nota del alumno 2"));
+// let alumnoTres = Number(prompt("Ingrese la nota del alumno 3"));
+
+// let sumaNotas = alumnoUno + alumnoDos + alumnoTres;
+// console.log(sumaNotas)
+
+// let promedio = sumaNotas / 3;
+// console.log("El promedio de los alumnos es " + promedio);
+
+// que ingrese un número entero y lo muestre multiplicado por 5 y luego debe mostrarse
+// dividido por 2
+
+let numero = Number(prompt("Ingrese un número"));
+let numeroMultiplicado = numero * 5;
+let numeroDividido = numeroMultiplicado / 2;
+
+console.log(numeroMultiplicado);
+console.log(numeroDividido);
